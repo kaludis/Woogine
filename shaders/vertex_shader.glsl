a77069cd1c1581e5495554783101ca89e97ff379
Alexander Bezsilko <demonsboots@gmail.com>
2016-02-06 00:58:02 +0300

    Move mvp matrix multiplication to vertex shader

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -1,12 +1,14 @@
 #version 120
 
-uniform mat4 mvp;
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
 attribute vec3 coord3d;
 attribute vec2 texcoord;
 varying vec2 f_texcoord;
 
 void main(void)
 {
-	gl_Position = mvp * vec4(coord3d, 1.0f);
+	gl_Position = projection * view * model * vec4(coord3d, 1.0f);
 	f_texcoord = texcoord;
 }
